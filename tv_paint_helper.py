2ea12f35438c079345d39e137f79d8f7cd210938
Nicolas Garcia <garciatunonnicolas@gmail.com>
2023-04-08 23:15:21 +0200

    Update tv_paint_helper.py

--- tv_paint_helper.py
+++ tv_paint_helper.py
@@ -1,4 +1,4 @@
-###This script is a workaround for maya  artists working with Team Viewer. 
+###This script is a workaround for maya artists working with Team Viewer. 
 ###As Team Viewer doesn’t send key pressed infos other than modifiers keys, 
 ###users cannot use some maya paint shortcuts. 
 ###Using this script users can increase/decrease paint brush values or soft selection radius value. 
